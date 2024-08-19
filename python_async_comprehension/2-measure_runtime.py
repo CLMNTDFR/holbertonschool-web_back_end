1eb71e3858cf4c2a35cfada09776ce763e120999
CLMNTDFR <deferclement59@gmail.com>
2024-08-19 12:01:57 +0200

    edit task 2 with correct module import

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,14 +2,19 @@
 
 """Measure the runtime of async_comprehension"""
 
+from typing import List
 import asyncio
+import time
+
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """Measure the runtime of async_comprehension"""
-    tasks = [async_comprehension() for _ in range(4)]
+    task = [async_comprehension() for _ in range(4)]
     start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(*tasks)
+    await asyncio.gather(*task)
     end_time = asyncio.get_event_loop().time()
-    return end_time - start_time
+    total_runtime = end_time - start_time
+    return total_runtime
