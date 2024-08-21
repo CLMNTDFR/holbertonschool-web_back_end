d21c28dda1d1290c338a761ce8dfe7a7305625c0
CLMNTDFR <deferclement59@gmail.com>
2024-08-21 08:52:53 +0200

    fix condition: assert page > 0 AND page_size > 0

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,52 +1,50 @@
 #!/usr/bin/env python3
-"""  a method named get_page that takes
-two integer arguments page with default
-value 1 and page_size with default value 10"""
+"""A module that paginates a dataset of popular baby names."""
 
 import csv
-import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page, page_size):
-    """return a tuple of size two
-    containing a start index and an
-    end index"""
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing a start and end index."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
+    return start_index, end_index
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cache the dataset after loading it from the CSV file."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+            self.__dataset = dataset[1:]  # Exclude the header row.
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ takes two integer arguments page with
-        default value 1 and page_size with
-        default value 10"""
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 or page_size > 0
-        mydataset = self.dataset()
-        indexes = index_range(page, page_size)
-        dataset_length = len(mydataset)
+        """Retrieve a page from the dataset."""
+        # Assert that both arguments are integers greater than 0
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
 
-        if indexes[0] < dataset_length and indexes[1] <= dataset_length:
-            return mydataset[indexes[0]:indexes[1]]
+        # Get the start and end indices
+        start_index, end_index = index_range(page, page_size)
 
-        return []
+        # Retrieve the dataset
+        dataset = self.dataset()
+
+        # If the start index is out of range, return an empty list
+        if start_index >= len(dataset):
+            return []
+
+        # Return the appropriate slice of the dataset
+        return dataset[start_index:end_index]
